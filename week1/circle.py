5c4dcf1fe79bf89396a7d0bbf650a4e287a8a52c
wjstud <wjepower@hotmail.com>
2018-04-26 11:31:00 +0800

    工资计算器读写数据文件

--- week1/circle.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from math import pi
-
-try:
-    r = int(input('Please enter the radius of the circle: '))
-    print(r * r * pi)
-except ValueError as e:
-    print('Please a number!!!')
-    exit()

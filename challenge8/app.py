b20b2c824956055724f663d9f27e84020ea7e08b
wjstud <wjepower@hotmail.com>
2018-04-26 14:45:32 +0800

    从mongo读取标签到网站

--- challenge8/app.py
+++ challenge8/app.py
@@ -1,18 +1,21 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+from pymongo import MongoClient
 
-''' 步骤：创建两张表 文章表files、类别表categories 关系为多对一
+''' 步骤：连接数据库 mysql、mongo
+          创建两张表 文章表files、类别表categories 关系为多对一
           创建测试数据 insert_datas函数使用 flask shell 导入并运行
 '''
 
 app = Flask(__name__)
 
 app.config.update(dict(
-    SQLALCHEMY_DATABASE_URI='mysql://root:123456@localhost/challenge7'
+    SQLALCHEMY_DATABASE_URI='mysql://root:123456@localhost/challenge8'
     ))
 
 db = SQLAlchemy(app)
+mongo = MongoClient('127.0.0.1', 27017).challenge8
 
 class File(db.Model):
     __tablename__ = 'files'
@@ -29,6 +32,39 @@ class File(db.Model):
         self.created_time = created_time
         self.category = category
         self.content = content
+
+    def add_tag(self, tag_name):
+        file_item = mongo.files.find_one({'file_id': self.id})
+        if file_item:
+            tags = file_item['tags']
+            if tag_name not in tags:
+                tags.append(tag_name)
+            mongo.files.update_one({'file_id': self.id}, {'$set': {'tags': tags}})
+        else:
+            tags = [tag_name]
+            mongo.files.insert_one({'file_id': self.id, 'tags': tags})
+        return tags
+
+    def remove_tag(self, tag_name):
+        file_item = mongo.files.find_one({'file_id': self.id})
+        if file_item:
+            tags = file_item['tags']
+            try:
+                tags.remove(tag_name) # 不能 new_tags = tags.remove(tag_name) 其返回为 None
+                new_tags = tags
+            except ValueError:
+                return tags
+            mongo.files.update_one({'file_id': self.id}, {'$set': {'tags': new_tags}})
+            return new_tags
+        return []
+
+    @property
+    def tags(self):
+        file_item = mongo.files.find_one({'file_id': self.id})
+        if file_item:
+            return file_item['tags']
+        else:
+            return []
 
 class Category(db.Model):
     __tablename__ = 'categories'
@@ -51,6 +87,11 @@ def insert_datas():
     db.session.add(file1)
     db.session.add(file2)
     db.session.commit()
+    file1.add_tag('tech')
+    file1.add_tag('java')
+    file1.add_tag('linux')
+    file2.add_tag('tech')
+    file2.add_tag('python')
 
 @app.route('/')
 def index():
